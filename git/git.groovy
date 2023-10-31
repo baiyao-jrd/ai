f4dd31a5adcea0d5038fcdb2e2d7a7676ca5cb74
baiyao <18303620306@163.com>
2023-10-31 22:50:46 +0800

    Hello

---
+++ git/git.groovy
@@ -0,0 +1,193 @@
+﻿// https://www.bilibili.com/video/BV1pX4y1S7Dq
+
+// 1. git是开源的分布式版本控制系统
+版本控制 -> 记录你在什么时候写了什么代码
+分布式 -> 区别于集中式 
+       -> 集中式是只有一个档案馆
+       -> 分布式是可以每一个人有个档案馆 -> 你自己管理自己的版本需要的时候可以拿着自己的版本和别人的做一个合并
+
+两个好处 -> ① 协同工作
+        -> ② 出事故之后可以精准的找人背锅
+
+// 2. gitee, github, gitlab, git的区别
+git的代码上传到gitee, github, gitlab等代码托管平台
+
+// 3. 使用git
+// 3.1 身份配置 -> 这个配置只是说明性的, 你想配啥就可以配啥 -> 这跟登录注册还是有区别的
+git config --global user.name "baiyao"
+git config --global user.email 18303620306@163.com
+
+// 3.2 新建项目
+新建文件夹 -> 在文件夹里面右键 -> Git Bash Here -> git init -> 不需要联网, 接着你就可以随便使用任何你想使用的git命令了
+
+// 3.3 克隆项目
+git clone https://github.com/....(项目的地址)
+
+// 3.4 修改我们的文件, 创建我们的版本
+当咱们创建好仓库之后, 咱们会赋予这个仓库每一个文件都有的状态.
+
+如果是新建的仓库, 那么里面的每个文件都是未被跟踪的. 当你生成一个版本后, 未被跟踪的文件是不会在这个版本里的. 他之前的状态以及他之前做的修改是不能被追溯的. 现在可以通过'git add <文件或者目录name>'来跟踪文件或者目录. 一般来说, 如果一个文件或者目录被跟踪, 那么它这辈子都是会被跟踪的. 如果不想它被跟踪的话, 可以通过'git rm <name>'删掉, 亦或者使用'git rm --cache <name>'让它保留在我们的目录里面, 但是不被跟踪.
+
+修改文件, 接着可以使用'git add <file-name>'将其状态设置成缓存状态. 取消设置成缓存状态, 'git reset HEAD <file-name>'. 如果说已经设置成缓存状态的话, 接着就可以使用'git commmit'命令来提交此次修改.
+
+总结, 文件的四个状态:
+    未跟踪 -> 未修改 -> 已修改 -> 暂存
+
+    未跟踪 -> (git add <name>) -> 暂存
+
+    未跟踪未修改 -> (git commit) -> 暂存(形成了一个新的版本, 文件此时已经变成了未修改已跟踪状态) -> (git rm <name>) -> 将其变成未跟踪状态
+
+// 3.5 'git commit'命令补充
+输入'git commit' -> 回车(会进入编辑器) -> i -> 输入此次要提交的信息 -> ESC, :wq -> 版本提交完成
+
+或者
+
+`git commit -m '本次修改的信息' `
+
+假设现在本不想提交了 -> 'git reset head~ --soft' -> 这个操作不能撤销第一次提交, 可能咱们提交了第二第三次, 但是第一次提交是不能被撤销的
+
+// 3.6 查看文件状态 -> 想看哪个文件有修改但是没有暂存? 哪个文件暂存了?
+'git status' -> 能够查看哪个文件是修改了但是未被暂存(终端显示是红色) -> 'git add <name>' -> 之后查看就是绿色了, 表示被修改并且已经被暂存了 -> 接着可以`git commit -m '巴拉巴拉'`提交一下, 让其成为一个新的版本 -> 接着'git status', 就能看到提示: On branch master nothing to commit, working tree clean -> 表示没有被修改未提交的了.
+
+// 3.7 想查看更细致的东西, 比如说想看看这个文件哪里被修改了, 第几行第几个字母
+'git diff'
+
+// 3.8 查看历史提交
+'git log'
+
+commit后面的一串字母数字组合就是历史提交的哈希值 -> 唯一的 -> 可以通过这个哈希值来找到某次的提交
+Author -> 后面是提交作者
+DAte -> 后面是提交时间, 包括后面还有咱们提交时的一些信息
+
+'git log --pretty' -> 美化输出 -> 'git log --pretty=oneline' -> 把每次提交的信息都变成一行
+                              -> 'git log --pretty=format:"%h-%an, %ar:%s"'自定义格式来让显示信息来符合我们的格式
+                                                        -> %h 简化哈希
+                                                        -> %an 作者名字
+                                                        -> %ar 修订日期(距今)
+                                                        -> %ad 修订日期
+                                                        -> %s  提交说明  
+
+'git log --graph' -> 让历史提交信息以分支图形化的方式呈现
+
+
+// 4. 远程仓库 / remote -> 讲解远程仓库的使用
+github为例 -> '+' -> 'New repository' -> Owner(baiyao)/Repository name(test) -> 设置公开或者私有(Public或者Private) -> Create repository 
+           -> HTTPS或者SSH复制远程仓库的链接地址 https://github.com/baiyao/test.git
+
+
+链接远程的仓库 -> git remote add origin(远程仓库的名字, 一般就叫这个) 'https://github.com/baiyao/test.git'
+              -> git remote -> 显示: remote -> 可以通过`git remote rename origin 'new_name'`来修改远程仓库的名字 -> 'git remote'查看新名字
+
+              想把本地代码推送到远程的仓库?
+
+              -> git push origin(选择的一个远程仓库) master(将本地的master分支给推送上去) -> 接着会让输入用户名和密码, 但是github已经禁止制种方式了 
+              
+              -> github头像 -> Settings -> 尾部Developer settings -> Personal access tokens -> Tokens(classic) 
+              
+              -> 需要新建令牌, 用令牌当密码'Generate new token' -> 'Generate new token(classic - 普通的, 经典的)'
+              -> Note: login(随便写) -> Expiration(有效期): 30days -> 勾选repo(就是把有关仓库的权限都给打开) -> Generate token
+
+              -> 复制生成的令牌
+
+              -> git push origin master 
+              'Username for https://github.com: baiyao'
+              'Password for https://baiyao@github.com: (此时粘贴复制的令牌)' -> 接着就上传成功了
+
+              -> 令牌鉴权不方便, 可以通过ssh方式进行鉴权 
+              -> cd ~/.ssh (这是进入用户的ssh目录) -> 进来之后 'ssh-keygen -t rsa -b 4096 -C "个人邮箱"'
+                                                 -> -t指定密钥生成算法, -b指定大小, -C添加评论-github推荐填写个人邮箱
+                                                 -> test -> ls -> test(私钥) test.pub(公钥) -> 查看公钥'cat test.pub' 复制公钥
+                                                 -> github头像 -> Settings -> SSH and GPG keys -> SSH keys 的 New SSH key
+                                                 -> key:(粘贴你复制的公钥内容) Title: test(随便起名字) -> add SSH key 
+
+
+                                                 -> 项目地址处 -> Code -> SSH -> 复制远程仓库地址
+                                                 -> git clone <刚刚复制的地址>
+
+// 5. branch -> 分支概念
+咱们每次提交生成新版本的时候都会生成一个新的提交对象
+
+(提交对象1-914cb) -> (提交对象2-31ac3) -> (提交对象3-ju9u6) 
+
+每一个提交对象都有一个独一无二的哈希值914cb, 31ac3, ju9u6等等
+
+其实分支就是包含这个哈希值的一个文件, 或者可以理解为指向一个提交对象的指针, 可以说是咱们能在一个提交对象上新建多个分支,
+因为分支是包含提交对象哈希值的一个文件, 所以想加多少就可以加多少. 
+
+咱们最开始在初始化本地仓库时就新建了一个master分支, 而咱们每次的提交操作都是在这个master分支上进行的, 当咱们提交的时候, 分支也会跟着提交对象一起
+向前移动
+
+    master      
+      ↓
+    首次提交对象 -> 第二次提交对象
+
+                      master      
+                        ↓
+    首次提交对象 -> 第二次提交对象  
+
+    // 接着咱们在master上继续做了修改, 然后提交一个新的提交对象
+
+                                      master      
+                                        ↓
+    首次提交对象 -> 第二次提交对象 -> 第三次提交对象
+
+
+    // 接着咱们在第二次提交对象那儿新建的分支上做修改并提交, 那么第二次提交对象就会分叉, 这两个叉就是两个分支
+
+
+// 6. 为什么新建这么多分支? 一个分支不好吗?
+feature分支(开发新特性) ->(将几个feature分支一块儿合并到develop分支)-> develop分支'手机不同的feature分支并对其进行测试, 没问题后就可以发布了, 接着新建release分支' -> release分支'功能验证没问题就可以把release分支合并到master分支' -> master分支
+                  -> hot fixes分支'修复bug用'
+
+// 7. 如何操作分支
+在操作分支之前, 咱们得知道咱们在哪个分支上 -> 'git log' -> commit 0dd28b...(历史提交的哈希) (HEAD -> master, origin/master)
+    -> 'HEAD -> master' -> 表明咱们在master分支上, HEAD表示现在所处的分之就在master分支上
+    -> 'origin/master' -> 咱们之前添加过origin远程仓库而这个远程仓库里面也有一个master
+
+-> 'git status' -> On branch master -> 表示现在就在master分支
+-> 'git branch --list' -> * master -> 前面带星的就是我们当前所处的分支, 由于现在只有一个master分支, 所以现在就在master分支上
+
+// 8. 创建分支
+'git branch feature1' -> 'git branch --list' -> feature1 
+                                                * master
+                                            -> 切换分支'git checkout feature1' -> -> 'git branch --list' -> * feature1 
+                                                                                                            master
+                                            -> vi code.txt 修改代码 -> 'git commit -am '巴拉巴拉''(-a将未暂存的文件都提交了)
+
+                                            -> git checkout master(切换回master分支) -> 新建分支并切到这个新建分支上'git checkout -b feature2'
+
+                                            -> vi code.txt 修改代码 -> 'git commit -am '巴拉巴拉''
+
+                                            -> 查看所有分支的提交'git log --all', 'git log --all --graph'
+
+                                            -> git checkout master(切换回master分支)
+
+
+// 9. 合并分支
+让master分支与feature1分支合并 -> 'git merge feature1'
+                            -> 'git merge feature1' 继续将master分支与feature2分支合并
+                            -> 发现会报冲突错误, 因为feature1分支与feature2分支修改了同一个地方, 会显示冲突文件
+                            -> vim 冲突文件
+
+                                <<<<<<< HEAD
+                                Laotie          // 这里就代表咱们当前分支对这个文件进行的修改
+                                =======
+                                Laotie666       // 这下面就代表想要合并进来的分支进行的修改
+                                laotiefeature2
+
+                                >>>>>>> feature2
+
+
+                                多余的东西都删掉, 保留下面的
+
+                                Laotie666      
+                                laotiefeature2
+
+                            -> git add code.txt -> git commit -m '' -> git log --all --graph
+
+                            -> git push -u origin master -> 将分支推送到远程 -> 以后想推送一样的话, 直接git push就行, 因为前面push的时候加了-u
+                            -> git push origin feature1 -> 可以将feature1分支推送上去
+
+                            -> git fetch 拉取远程在本地没有的分支
+
+
---
+++ redis/redis_tutorial.groovy
@@ -0,0 +1,485 @@
+﻿课程地址: https://www.bilibili.com/video/BV1cr4y1671t?p=1&vd_source=badfee45524e073e1fe3740bd3ae835b
+
+ReDiS -> Remote Dictionary Server -> 远程词典服务器 -> 由key拿value, 就是查字典的方式
+
+1. redis -> 存储键值对儿 -> 键值数据库 -> NoSql数据库
+         -> key: 1001 -> 用来存储用户id
+         -> value: jsonObject -> 用来存储用户信息 -> { 
+                                                        "id": 1001,
+                                                        "name": "张三",
+                                                        "age": 21
+                                                    } 
+
+2. SQL与NoSql
+SQL -> 关系型数据库 -> structured query language -> 国内简写SQL: 瑟扣 -> 国外简写SEQL: sei quo
+
+NoSql -> 非关系型数据库
+
+3. 结构化
+这个说的其实就是建表语句规则, 比如定义一条数据中有哪些字段, 每个字段类型以及是否为主键, 是否非负
+
+结构化的问题是, 你建表后, 存的数据体量大的情况下, 更改表结构会影响到业务 -> 一般情况下是建表之后尽量不建议修改表结构信息
+
+4. 非结构化存储的几种形式
+    1) 键值类型(Redis): key-value形式存储数据
+    2) 文档类型(MongoDB) -> 比如json -> { "id": 1001, "name": "张三", "age": 21 }
+    3) Graph图类型(Neo4j) -> 节点与线组成, 节点是对象, 线代表节点间联系
+    4) 列类型(Hbase)
+
+5. 对象之间的关系
+    1) SQL -> 使用外键存储对象间关系
+        tb_user                     tb_item
+        id    name    age           id    title    price
+        1     张三    18            10    荣耀6     4999
+        2     李四    20            20    小米11    3999
+
+        -> 使用另一张表, 存储表数据与表数据之间的关系 -> 使用外键
+        tb_order
+        id    user_id    item_id
+        1     1          10
+        2     1          20
+    2) NoSql -> 没有专门的存储对象间关系(也就是说, 数据库没有专门帮你维护数据间的关系, 需要你写程序自己维护) 
+             -> 比较常见的是, 使用json形式之间存储数据, 某个人下了那些订单, 每个订单里面商品信息是啥
+        {
+            "id": 1,
+            "name": "张三",
+            "orders": [
+                {
+                    "id": 1,
+                    "item": { "id": 10, "title": "荣耀6", "price": 4999}
+                },
+                {
+                    "id": 2,
+                    "item": { "id": 20, "title": "小米11", "price": 3999}
+                }
+            ]
+        }
+
+6. SQL与NoSQL取值查询方式
+    1) SQL -> 统一使用sql语句 -> select
+                                    id,
+                                    name,
+                                    age
+                                from tb_user 
+                                where id = 1;
+
+    2) NoSQL -> 不统一 -> 常见三种
+        Redis -> get user:1
+        MongoDB -> db.users.find({_id: 1}) -> 基于函数式的调用
+        elasticsearch -> GET http://localhost:9200/users/1 -> 基于请求的形式调用 
+
+7. SQL与NoSQL在事务上的不同 -> A(原子性), C(一致性), I(隔离性), D(持久性)
+    1) SQL -> 所有的关系型数据库底层帮助维护事务 -> 所有的关系型数据库都是满足ACID的
+
+    2) NoSQL -> 要不没有事务, 要不不能满足强一致性, 只能满足基本的一致性
+
+    如果说你对数据安全性, 一致性要求比较高就应该关系型数据库而不是非关系型数据库
+
+8. 总结SQL与NoSQL数据库的差异
+    SQL ->  数据是结构化的;
+            数据与数据之间通过外键表示关系; 
+            通过SQL查询数据; 
+            满足事务ACID;
+            存储数据也用缓存但是主要用磁盘存数据;
+            数据库不能天然支持水平扩展, 想扩展得依赖于第三方组件, 这势必会影响性能(主从架构只是提升了读写性能, 并不能提升数据容量, 因为主从的数据一样, 想提升查询性能就得在单机上做文章, 优化单机性能)
+
+    NoSQL -> 数据是非结构化的; 
+             数据与数据之间是没有关系的; 
+             不能通过sql方式查询;
+             事务方面满足BASE, 或者没事务, 或者只能满足基本的一致性或者说最终的一致性, 存储数据用内存(查询就是快);
+             天然支持水平扩展, 通过加机器来提升存储容量和计算性能, 它会将数据key哈希之后确定分布在哪一台机子上 
+
+9. 两者使用场景
+    SQL -> 存储数据结构固定的数据, 对安全性和一致性要求高的
+
+    NoSQL -> 数据结构不固定, 对安全性和一致性要求不高的, 但是对性能要求高的
+
+10. Redis的特征
+    1) 键值型 -> value类型String、List、Set、Zset、Hash.
+    2) 单线程, 是线程安全的, 所有命令串行执行, 不会说出现某个命令执行的时候, 其他命令的任务插进来这种不安全的情况, 所以说它的命令是原子性的. 
+       注意, 6.0版本多线程只是在处理网络请求的时候会有, 核心的命令执行还是串行单线程的
+    3) 为什么他是单线程但是性能却还是那么好呢(低延迟, 速度快)? 
+        相较于数据存在磁盘上的SQL数据库来说, Redis这个NoSQL数据库的数据是存在内存中的, 这效率肯定大大提升 -> 这是这三个理由中最核心的理由
+        另外, 虽然是单线程但是它基于IO多路复用, 效率高
+        最后呢, redis是C写的, 而且它的源码编写的非常优秀规范, 标杆级别的
+    4) 由于数据存在内存中很危险, 所以增加了持久化功能, 定期将数据持久化
+    5) 主从集群 -> 从节点备份主节点数据, 那么主节点宕机, 可以直接从从节点读取数据, 这是一种安全策略
+               -> 主从支持读写分离, 这可以大大增加读写效率
+
+        分片集群 -> 将数据拆分 -> 比如说, 1TB数据, 我把它拆成n份儿来存到不同的节点上去 -> 这样就提升了存储的上限 -> 属于水平扩展
+    6) 支持多语言客户端 -> 就是你各种各样的语言我都有相应的客户端来让你操作redis存取数据
+
+    企业不用它没理由啊 -> 功能丰富, 性能好, 支持数据持久化(安全性), 另外能轻易水平扩展, 任何语言都能用
+       
+11. redis的安装
+    linux安装gcc依赖 -> 由于redis基于C编写 -> yum install -y gcc tcl
+    上传安装包并解压 -> 
+        [baiyao@zoo1 redis-6.2.6]$ sudo mkdir /usr/local/src/redis
+        [baiyao@zoo1 redis-6.2.6]$ sudo tar -zxvf ./redis-6.2.6.tar.gz -C /usr/local/src/redis/
+        // 进入主目录, 编译并安装
+        [baiyao@zoo1 redis-6.2.6]$ sudo cd /usr/local/src/redis/redis-6.2.6/ && sudo make && sudo make install
+        // 默认的安装路径是 /usr/local/bin, 所以安装完成之后检查下有没有安装成功
+        [baiyao@zoo1 redis-6.2.6]$ cd /usr/local/bin/ && ll -l ./
+        总用量 18904
+        -rwxr-xr-x. 1 root root 4829536 7月  16 10:34 redis-benchmark
+        lrwxrwxrwx. 1 root root      12 7月  16 10:34 redis-check-aof -> redis-server
+        lrwxrwxrwx. 1 root root      12 7月  16 10:34 redis-check-rdb -> redis-server
+        -rwxr-xr-x. 1 root root 5003816 7月  16 10:34 redis-cli                             -> redis命令行客户端
+        lrwxrwxrwx. 1 root root      12 7月  16 10:34 redis-sentinel -> redis-server        -> redis的哨兵启动脚本
+        -rwxr-xr-x. 1 root root 9518936 7月  16 10:34 redis-server                          -> 启动redis服务的脚本
+
+        redis-server已经加入环境变量了, 可以直接运行
+
+        [baiyao@zoo1 bin]$ redis-server 
+        57703:C 16 Jul 2023 10:40:58.345 # oO0OoO0OoO0Oo Redis is starting oO0OoO0OoO0Oo
+        57703:C 16 Jul 2023 10:40:58.345 # Redis version=6.2.6, bits=64, commit=00000000, modified=0, pid=57703, just started
+        57703:C 16 Jul 2023 10:40:58.345 # Warning: no config file specified, using the default config. In order to specify a config file use redis-server /path/to/redis.conf
+        57703:M 16 Jul 2023 10:40:58.347 * monotonic clock: POSIX clock_gettime
+                        _._                                                  
+                _.-``__ ''-._                                             
+            _.-``    `.  `_.  ''-._           Redis 6.2.6 (00000000/0) 64 bit
+        .-`` .-```.  ```\/    _.,_ ''-._                                  
+        (    '      ,       .-`  | `,    )     Running in standalone mode
+        |`-._`-...-` __...-.``-._|'` _.-'|     Port: 6379
+        |    `-._   `._    /     _.-'    |     PID: 57703
+        `-._    `-._  `-./  _.-'    _.-'                                   
+        |`-._`-._    `-.__.-'    _.-'_.-'|                                  
+        |    `-._`-._        _.-'_.-'    |           https://redis.io       
+        `-._    `-._`-.__.-'_.-'    _.-'                                   
+        |`-._`-._    `-.__.-'    _.-'_.-'|                                  
+        |    `-._`-._        _.-'_.-'    |                                  
+        `-._    `-._`-.__.-'_.-'    _.-'                                   
+            `-._    `-.__.-'    _.-'                                       
+                `-._        _.-'                                           
+                    `-.__.-'                                               
+
+        57703:M 16 Jul 2023 10:40:58.350 # WARNING: The TCP backlog setting of 511 cannot be enforced because /proc/sys/net/core/somaxconn is set to the lower value of 128.
+        57703:M 16 Jul 2023 10:40:58.350 # Server initialized
+        57703:M 16 Jul 2023 10:40:58.350 # WARNING overcommit_memory is set to 0! Background save may fail under low memory condition. To fix this issue add 'vm.overcommit_memory = 1' to /etc/sysctl.conf and then reboot or run the command 'sysctl vm.overcommit_memory=1' for this to take effect.
+        57703:M 16 Jul 2023 10:40:58.350 * Ready to accept connections
+
+        -> 像这种就是直接在前台启动了, 卡在了界面上, 你要是使用ctrl + z, 那么redis服务就停止了, 这种启动方式不友好
+
+    -> 修改配置文件, 通过配置文件的指定来实现后台启动
+        [baiyao@zoo1 bin]$ cd ../src/redis/redis-6.2.6/
+        [baiyao@zoo1 redis-6.2.6]$ sudo cp ./redis.conf ./redis.conf.bak
+        [baiyao@zoo1 redis-6.2.6]$ sudo vim ./redis.conf
+            // 监听地址, 默认是127.0.0.1 -> 也就是本机, 只有收到你本机的访问请求才响应, 现在改成任意ip都可访问, 生产环境不能这样 
+            bind 0.0.0.0
+            // 守护进程
+            daemonize(笛闷耐Z) yes
+            // 密码, 任意ip访问需密码
+            requirepass 111111.a
+            // 监听端口
+            port 6379
+            // 工作目录, 启动redis-server服务的目录, '.'代表当前, redis-server在哪儿启动的, 它的工作目录就会在哪儿
+            dir .
+            // 数据库数量默认是16个, 编号是0~15, 这里是设的1, 代表只用一个库
+            databases 1
+            // redis能使用的最大内存
+            maxmemory 512mb
+            // 日志文件, 默认是空 -> 也就是不记录日志, 这里指定文件名, 路径呢就是你的'.' -> 工作目录 -> redis-server在哪儿启动的, 它的工作目录就会在哪儿
+            logfile "redis.log"
+        [baiyao@zoo1 redis-6.2.6]$ su -
+        密码：
+        上一次登录：四 7月 13 06:29:08 CST 2023pts/1 上
+        [root@zoo1 ~]# cd /usr/local/src/redis/redis-6.2.6/ && redis-server ./redis.conf
+        [root@zoo1 redis-6.2.6]# ls -l
+        -rw-rw-r--.  1 root root 93798 7月  16 11:16 redis.conf
+        -rw-r--r--.  1 root root 93724 7月  16 10:56 redis.conf.bak
+        -rw-r--r--.  1 root root   541 7月  16 11:20 redis.log
+
+        [root@zoo1 redis-6.2.6]# ps -ef | grep redis
+        baiyao    57703  53063  0 10:40 pts/0    00:00:01 redis-server *:6379
+
+        可以通过kill -9 57703来关闭redis 或者 -> 
+        redis-cli -u 111111.a shutdown
+
+12. redis的开机自启
+    -> 需要编写一个系统服务文件来把redis加入到系统服务中, 
+        [root@zoo1 redis-6.2.6]# vim /etc/systemd/system/redis.service
+        [Unit]
+        Description=redis-server
+        After=network.target
+
+        [Service]
+        Type=forking
+        ExecStart=/usr/local/bin/redis-server /usr/local/src/redis/redis-6.2.6/redis.conf
+        PrivateTmp=true
+
+        [Install]
+        WantedBy=multi-user.target
+    -> 重新加载系统服务, 接着重新启动redis
+        // 被系统管理
+        [root@zoo1 redis-6.2.6]# systemctl daemon-reload
+        [root@zoo1 redis-6.2.6]# systemctl start redis
+
+        [root@zoo1 redis-6.2.6]# systemctl status redis
+            ● redis.service - redis-server
+            Loaded: loaded (/etc/systemd/system/redis.service; disabled; vendor preset: disabled)
+            Active: active (running) since 日 2023-07-16 11:40:26 CST; 4s ago
+            Process: 57866 ExecStart=/usr/local/bin/redis-server /usr/local/src/redis/redis-6.2.6/redis.conf (code=exited, status=0/SUCCESS)
+            Main PID: 57867 (redis-server)
+            CGroup: /system.slice/redis.service
+                    └─57867 /usr/local/bin/redis-server 0.0.0.0:6379
+
+            7月 16 11:40:26 zoo1 systemd[1]: Starting redis-server...
+            7月 16 11:40:26 zoo1 systemd[1]: Started redis-server.
+        // 可以通过这个停止或者重启redis
+        [root@zoo1 redis-6.2.6]# systemctl stop/restart redis
+
+        // 设置开机自启
+        [root@zoo1 redis-6.2.6]# systemctl enable redis
+
+13. 怎么跟redis进行交互? -> 也就是怎么使用redis客户端?
+    1) 命令行客户端:
+        [root@zoo1 redis-6.2.6]# redis-cli -a 111111.a
+        Warning: Using a password with '-a' or '-u' option on the command line interface may not be safe.
+        127.0.0.1:6379> 
+        [root@zoo1 redis-6.2.6]# redis-cli -h 127.0.0.1 -p 6379
+        127.0.0.1:6379> ping
+        (error) NOAUTH Authentication required.
+        127.0.0.1:6379> 
+        [root@zoo1 redis-6.2.6]# redis-cli -h 127.0.0.1 -p 6379 -a 111111.a
+        Warning: Using a password with '-a' or '-u' option on the command line interface may not be safe.
+        127.0.0.1:6379> ping
+        PONG
+
+        [root@zoo1 redis-6.2.6]# redis-cli -a 111111.a
+        Warning: Using a password with '-a' or '-u' option on the command line interface may not be safe.
+        127.0.0.1:6379> 
+
+        // 命令行输入密码不安全, 换种方式
+        [root@zoo1 redis-6.2.6]# redis-cli
+        127.0.0.1:6379> AUTH 111111.a
+        OK
+
+        // 测试往redis里面存一下key-value键值对 -> set key value
+        127.0.0.1:6379> set name baiyao
+        OK
+        127.0.0.1:6379> set age 27
+        OK
+        127.0.0.1:6379> get name
+        "baiyao"
+        127.0.0.1:6379> get age
+        "27"
+
+        // 0~15号库, 默认是存到了0号库, 怎么切换?
+        127.0.0.1:6379> select 1
+        OK
+        127.0.0.1:6379[1]> set name jrd
+        OK
+        127.0.0.1:6379[1]> get name
+        "jrd"
+        127.0.0.1:6379[1]> select 0
+        OK
+        127.0.0.1:6379> get name
+        "baiyao"
+        127.0.0.1:6379> 
+
+    2) 图形化界面客户端:
+        github.com/uglide/RedisDesktopManager
+
+        github.com/lework/RedisDesktopManager-Windows/releases
+
+        -> 直接下载这个 -> https://github.com/lework/RedisDesktopManager-Windows/releases/download/2021.9.4/rdm-2021.9.4.zip
+
+        是一个.exe文件, 下载下来直接安装就行了
+
+        -> 配置
+        连接设置 -> 名字: 随便起 -> 地址: ip -> 密码: 111111.a
+
+    3) 编程客户端: 
+
+14. Redis的常见数据结构
+key是string, value类型这里例举八种, 不止这么多, 当然最常见的还是前五种(这5种是基本数据类型): 
+    1) 基本类型
+    -> String       -> hello
+    -> Hash         -> {name: "baiyao", age: 27} -> 本质是一个哈希表
+    -> List         -> [A -> B -> C -> C]        -> 本质是一个链表
+    -> Set          -> {A, B, C}                 -> 无序集合, 不可重复
+    -> SortedSet    -> {A: 1, B: 2, C: 3}        -> 有序集合, 不可重复
+
+    2) 基本类型基础上处理后得到的 -> 特殊类型 -> 实现特殊的功能
+    -> GEO          -> {A: (120.3, 30.5)}        -> 地理坐标
+    -> BitMap       -> 0110110101110101011       -> 按位存储, 本质是字符串
+    -> HyperLog     -> 0110110101110101011       -> 按位存储, 本质是字符串
+
+-> redis的帮助文档 -> https://redis.io/commands/
+
+-> 客户端处查看:
+    127.0.0.1:6379> help @generic       -> 这个是查看通用的命令
+                         @string
+                         @list
+                         @set
+                         @sorted_set
+                         @hash
+                         @pubsub  
+                         @transactions  
+                         @connection  
+                         @server  
+                         @scripting     
+                         @hyperloglog
+                         @cluster
+                         @geo
+                         @stream     
+
+    当然官方文档介绍的会更详细一些
+
+15. Redis的通用命令 -> 对任何数据类型都可以使用的命令 -> 127.0.0.1:6379> help @generic -> 下面例举几个常用的命令
+    1) keys -> 查看所有符合指令模板的key
+            -> 通过help命令查找keys的用法
+
+            127.0.0.1:6379> help keys
+
+                KEYS pattern
+                summary: Find all keys matching the given pattern
+                since: 1.0.0
+                group: generic
+
+            127.0.0.1:6379> 
+
+            redis> KEYS *name*
+            1) "firstname"
+            2) "lastname"
+            redis> KEYS a??
+            1) "age"
+            redis> KEYS *
+            1) "age"
+            2) "firstname"
+            3) "lastname"
+            redis> 
+            
+            pattern -> redis内置的通配规则
+
+            h?llo       -> matches hello, hallo and hxllo
+            h*llo       -> matches hllo and heeeello
+            h[ae]llo    -> matches hello and hallo, but not hillo
+            h[^e]llo    -> matches hallo, hbllo, ... but not hello
+            h[a-b]llo   -> matches hallo and hbllo
+
+
+            // 查询所有key
+            127.0.0.1:6379> keys *
+            1) "name"
+            2) "age"
+            // 查询以a开头的key
+            127.0.0.1:6379> keys a*
+            1) "age"
+
+            // 注意, redis的这种通配查询是模糊查询, 当你redis里面存的数据量很大的时候, 这个操作是很费资源的, 
+            // 由于是单线程的, 这个模糊查询在数据量大的情况下很可能会阻塞redis服务, 所以在生产环境下是不建议使用keys来进行查询的
+    2) del -> 删除指定的key
+        127.0.0.1:6379> help del
+
+            DEL key [key ...]       -> 这个[]意思是你能同时指定多个key让我去删除
+            summary: Delete a key
+            since: 1.0.0
+            group: generic
+
+        127.0.0.1:6379> del age
+        (integer) 1
+        127.0.0.1:6379> keys *
+        1) "name"
+
+
+        127.0.0.1:6379> set age 27
+        OK
+        127.0.0.1:6379> set sex 男
+        OK
+        127.0.0.1:6379> set mail 18303620306@163.com
+        OK
+        127.0.0.1:6379> keys *
+        1) "mail"
+        2) "name"
+        3) "sex"
+        4) "age"
+        127.0.0.1:6379> del mail name sex
+        (integer) 3
+        127.0.0.1:6379> keys *
+        1) "age"
+
+        [root@zoo1 redis-6.2.6]# redis-cli -h zoo1
+        zoo1:6379> auth 111111.a
+        OK
+        zoo1:6379> 
+
+        // 批量插入键值对
+        zoo1:6379> mset name baiyao sex man mail baiyao@123.com
+        OK
+        zoo1:6379> keys *
+        1) "mail"
+        2) "name"
+        3) "sex"
+        4) "age"
+
+        // 删除不存在的key -> 返回值是0 -> 库内不受影响 -> 整数值代表删除的数量
+        zoo1:6379> del bucunzai
+        (integer) 0
+    
+    3) exists: 判断key是否存在 -> 1: 存在, 0: 不存在
+        zoo1:6379> exists name
+        (integer) 1
+        zoo1:6379> exists position  -> 这个key是不存在的
+        (integer) 0
+        zoo1:6379> exists age name sex nihao    -> 传入多个key判断是否存在 -> 只存在3个, nihao这个key是不存在的
+        (integer) 3
+
+    4) expire -> 使用它给某个key设定一个有效期, 到期的时候这个key会被自动删除
+              -> 由于基于内存, 你不设有效期, 时间一长内存就满了
+
+            zoo1:6379> help expire
+                // EXPIRE key seconds
+                // summary: Set a key's time to live in seconds
+                // since: 1.0.0
+                // group: generic
+
+            ##########################################
+
+            zoo1:6379> keys *
+            1) "mail"
+            2) "name"
+            3) "sex"
+            4) "age"
+            zoo1:6379> set position bigdata
+            OK
+            zoo1:6379> expire position 10
+            (integer) 1
+            zoo1:6379> keys * 
+            1) "mail"
+            2) "name"
+            3) "age"
+            4) "sex"
+            5) "position"
+            zoo1:6379> keys * 
+            1) "mail"
+            2) "name"
+            3) "age"
+            4) "sex"
+
+            ########################################## 
+
+    5) ttl -> 查看一个key的剩余有效期
+
+        zoo1:6379> ttl name
+        (integer) -1                -> -1代表永久有效
+        zoo1:6379> expire name 10
+        (integer) 1
+        zoo1:6379> ttl name
+        (integer) 8
+        zoo1:6379> ttl name
+        (integer) 6
+        zoo1:6379> ttl name
+        (integer) 5
+        zoo1:6379> ttl name
+        (integer) 4
+        zoo1:6379> ttl name
+        (integer) 4
+        zoo1:6379> ttl name
+        (integer) 2
+        zoo1:6379> ttl name
+        (integer) 1
+        zoo1:6379> ttl name
+        (integer) -2                -> -2代表已被删除
+        zoo1:6379> ttl name
+        (integer) -2
